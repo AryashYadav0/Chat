e317be8e7025cc9ceaaabac22ef8bf4b79759e6d
AryashYadav0 <aryashyadav0680@gmail.com>
2026-02-13 10:57:47 +0530

    login refactoring ..

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -77,7 +77,7 @@ export const login = async (req, res, next) => {
     const { email, password } = req.body;
 
     if(!email || !password){
-        return res.status(400).json({message:" Email and Password are required "})
+        return res.status(400).json({message:" Email and Password are required "});
     }
     try {
         const user = await User.findOne({ email })
