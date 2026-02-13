737766f80437f374a93cd049b40f4572ab71c805
AryashYadav0 <aryashyadav0680@gmail.com>
2026-02-13 22:02:00 +0530

    rate limiting added

--- BackEnd/src/lib/env.js
+++ BackEnd/src/lib/env.js
@@ -3,9 +3,14 @@ import "dotenv/config";
 export const ENV ={
 
     PORT: process.env.PORT,
+    
+    // Database
     MONGO_URI: process.env.MONGO_URI,
     NODE_ENV: process.env.NODE_ENV ,
+    // JWT
     JWT_SECRET: process.env.JWT_SECRET,
+
+    // Resend
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
     EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
@@ -17,4 +22,7 @@ export const ENV ={
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
     
+    // Arcjet  
+    ARCJET_KEY: process.env.ARCJET_KEY,
+    ARCJET_ENV: process.env.ARCJET_ENV,
 }
