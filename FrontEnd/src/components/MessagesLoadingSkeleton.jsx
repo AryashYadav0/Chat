c67277d6a637062bb49a01f20979f609c255de42
AryashYadav0 <aryashyadav0680@gmail.com>
2026-02-22 22:13:49 +0530

    updated

--- FrontEnd/src/components/MessagesLoadingSkeleton.jsx
+++ FrontEnd/src/components/MessagesLoadingSkeleton.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MessagesLoadingSkeleton() {
   return (
-    <div>MessagesLoadingSkeleton</div>
+    <div className="">MessagesLoadingSkeleton
+        loading
+    </div>
   )
 }
 
